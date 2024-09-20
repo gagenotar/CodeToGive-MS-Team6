b559d86f01118043472da66b286f9149723932c2
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-09-20 23:27:33 +0000

    Added some forms

---
+++ my-react-app/src/components/Form.jsx
@@ -0,0 +1,21 @@
+
+function FormContents() {
+  return (
+    <div>
+      <div>
+        <label>
+          Email:
+          <input type="text" name="name" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default FormContents;
--- my-react-app/src/pages/Login.jsx
+++ my-react-app/src/pages/Login.jsx
@@ -1,5 +1,31 @@
-function Login() {
-    return <div>Login</div>
+import React, { useState, useEffect } from 'react';
+import api from '../api';
+
+const Login = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const response = await api.post('/login/', { email, password });
+        console.log(response.data);
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
-export default Login
+export default Login;

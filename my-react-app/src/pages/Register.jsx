111c73d25253291bd629d06be155dbd12c34d485
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-09-19 20:10:50 -0400

    Initial commit

--- my-react-app/src/pages/Register.jsx
+++ my-react-app/src/pages/Register.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Form from '../components/Form';
 import api from '../api';
 import RegisterForm from "../components/RegisterForm"
 import alpfaLogo from "../img/alpfaLogo.png";

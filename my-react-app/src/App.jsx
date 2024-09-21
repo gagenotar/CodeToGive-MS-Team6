e6ef1412f8a67f6ad23ff68d0adb7372a40d4b1d
Gage Notargiacomo <gagenotar@gmail.com>
2024-09-21 17:43:39 -0400

    Added new admin pages

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
+import LoginAdmin from "./pages/AdminLogin"
+import RegisterAdmin from "./pages/AdminRegister"
 import Admin from "./pages/Admin"
 
 function Logout() {
@@ -16,14 +18,21 @@ function RegisterAndLogout() {
   return <Register />
 }
 
+function RegisterAndLogoutAdmin() {
+  localStorage.clear()
+  return <RegisterAdmin />
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}/>
-        <Route path="/logout" element={<Logout />}/>
         <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="/logout" element={<Logout />}/>
         <Route path="/home" element={<Home />}/>
+        <Route path="/login/admin" element={<LoginAdmin />}/>
+        <Route path="/register/admin" element={<RegisterAndLogoutAdmin />}/>
         <Route path="/admin" element={<Admin />}/>
         <Route path="/profile" element={<Profile />}/>
       </Routes>

de065cf83f8ca38cd65d847cb77d204dfe2ade00
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-09-22 01:56:44 +0000

    Fixed the download resume thingy. Works with google drive id, not any shared link.

--- Backend/Pyresparser/download_resume.py
+++ Backend/Pyresparser/download_resume.py
@@ -19,14 +19,17 @@ def download_resume(url: str, file_name=None):
 
 # Function to download a PDF from a Google Drive link
 def download_pdf_from_drive(drive_link: str, file_path: str):
+    drive_id = drive_link.split("/d/")[-1]
+    drive_url = f"https://drive.google.com/uc?id={drive_id}"
     try:
-        response = requests.get(drive_link, timeout=10)
+        response = requests.get(drive_url, stream=True, timeout=10)
         if response.status_code == 200:
             with open(file_path, 'wb') as file:
-                file.write(response.content)
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
         else:
             raise HTTPException(status_code=400, detail="Failed to download the PDF.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred while downloading the PDF: {str(e)}")
 
-# download_pdf_from_drive("https://drive.google.com/file/d/1NYZglzd10wyNYlzkpF2k6uA05HYlJdIw", "Data/test_resume/hello.pdf")
+download_pdf_from_drive("https://drive.google.com/file/d/1NYZglzd10wyNYlzkpF2k6uA05HYlJdIw", "Data/test_resume/fixed.pdf")

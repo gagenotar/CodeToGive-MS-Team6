77391f8473725639e4d90df18f741d90ee25f390
Justis Nazirbage <103616292+mk3justis@users.noreply.github.com>
2024-09-22 19:34:06 -0400

    Cleaned up parsing script

--- Backend/Pyresparser/resume_parser_v2.py
+++ Backend/Pyresparser/resume_parser_v2.py
@@ -6,37 +6,6 @@ from fastapi import HTTPException
 from typing import Optional
 import re
 
-# Function to download a PDF from a Google Drive link
-def download_pdf_from_drive(drive_link: str, file_path: str):
-    try:
-        response = requests.get(drive_link, stream=True)
-        if response.status_code == 200:
-            with open(file_path, 'wb') as file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    file.write(chunk)
-        else:
-            raise HTTPException(status_code=400, detail="Failed to download the PDF.")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred while downloading the PDF: {str(e)}")
-
-# Function to parse a PDF and extract resume data
-def parse_pdf(file_path: str) -> dict:
-    try:
-        data = ResumeParser(file_path).get_extracted_data()
-        return {
-            'skills': data.get('skills'),
-            'experience': data.get('experience'),
-        }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred while parsing the resume: {str(e)}")
-    
-
-# temp_pdf_path = 'temp_resume_test.pdf'
-# resume_link = 'https://drive.google.com/file/d/1gjYozBUTNN_X9N9LfCy251-QFkKrkjeA/view?usp=drive_link'
-
-# download_pdf_from_drive(resume_link, temp_pdf_path)
-
-
 def get_drive_file_id(drive_link: str) -> str:
     """
     Extract the file ID from the Google Drive shareable link.
@@ -47,7 +16,8 @@ def get_drive_file_id(drive_link: str) -> str:
     else:
         raise ValueError("Invalid Google Drive link format. Could not extract file ID.")
 
-def download_pdf_from_drive2(drive_link: str, file_path: str):
+# Function to download a PDF from a Google Drive link
+def download_pdf_from_drive(drive_link: str, file_path: str):
     file_id = get_drive_file_id(drive_link)
     download_url = f"https://drive.google.com/uc?id={file_id}&export=download"
 
@@ -68,7 +38,20 @@ def download_pdf_from_drive2(drive_link: str, file_path: str):
     except Exception as e:
         print(f"An error occurred while downloading the PDF: {str(e)}")
 
+# Function to parse a PDF and extract resume data
+def parse_pdf(file_path: str) -> dict:
+    try:
+        data = ResumeParser(file_path).get_extracted_data()
+        return {
+            'skills': data.get('skills'),
+            'experience': data.get('experience'),
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred while parsing the resume: {str(e)}")
+
+
 # Example usage
 drive_link = "https://drive.google.com/file/d/1gjYozBUTNN_X9N9LfCy251-QFkKrkjeA/view?usp=drive_link"
 temp_pdf_path = "temp_resume_test.pdf"
-download_pdf_from_drive2(drive_link, temp_pdf_path)
+download_pdf_from_drive(drive_link, temp_pdf_path)
+parsing = parse_pdf(temp_pdf_path)

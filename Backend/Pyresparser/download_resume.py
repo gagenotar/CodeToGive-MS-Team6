933996f2acd298b69239e7dffaa0a3b1a15e90d4
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-09-22 01:26:50 +0000

    download_resume implementation

---
+++ Backend/Pyresparser/download_resume.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime
+
+
+punctuations = [' ', ':', '.']
+
+def download_resume(url: str, file_name=None):
+    file_name = file_name or f"{datetime.now()}.pdf"
+    for p in punctuations:
+        file_name = file_name.replace(p, '-')
+    try:
+        with open(f"resumes/{file_name}", "wb") as file:
+            file.write(requests.get(url, timeout=10).content)
+    except:
+        return False
+    return True
+
+# Function to download a PDF from a Google Drive link
+def download_pdf_from_drive(drive_link: str, file_path: str):
+    try:
+        response = requests.get(drive_link, timeout=10)
+        if response.status_code == 200:
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+        else:
+            raise HTTPException(status_code=400, detail="Failed to download the PDF.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred while downloading the PDF: {str(e)}")
+
+download_resume("https://drive.google.com/file/d/1NYZglzd10wyNYlzkpF2k6uA05HYlJdIw/view?usp=drive_link")

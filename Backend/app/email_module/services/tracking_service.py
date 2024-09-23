1a2f7a3819f58d0963ace4322f8b4747f503c2b0
Gage Notargiacomo <gagenotar@gmail.com>
2024-09-22 22:12:03 -0400

    Removed email module

--- Backend/app/email_module/services/tracking_service.py
+++
@@ -1,4 +0,0 @@
-class TrackingService:
-    @staticmethod
-    def generate_tracking_pixel(email_id):
-        return f'<img src="https://tracking-url.com/track?email_id={email_id}" width="1" height="1" alt="">'
--- Backend/app/email_module/tests/test_tracking_service.py
+++
@@ -1,7 +0,0 @@
-from ..services.tracking_service import TrackingService
-
-def test_generate_tracking_pixel():
-    email_id = "12345"
-    tracking_pixel = TrackingService.generate_tracking_pixel(email_id)
-    assert True
-    #assert tracking_pixel == f'<img src="https://tracking-url/track?email_id={email_id}" width="1" height="1" alt="">'
